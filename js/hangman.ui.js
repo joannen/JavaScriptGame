4ada048655251849dc6abccaf67505285f290b9f
Mattias Norling <mattias@norling.nu>
2016-09-15 14:19:30 +0200

    Added

--- js/hangman.ui.js
+++ js/hangman.ui.js
@@ -12,3 +12,15 @@ hangman.ui.guessPrompt = function() {
     var guess = prompt('Please guess a letter or word: ');
     return guess;
 };
+
+hangman.ui.winAlert = function() {
+    alert('Yeay you won this awesome game, yeay :)');
+};
+
+hangman.ui.defeateAlert = function() {
+    alert('Neay you lost this awesome game, you suck :/');
+};
+
+hangman.ui.notValidGuessAlert = function() {
+    alert('Not a letter or wrong word lenght. Seriousley how stupid are you?');
+};
